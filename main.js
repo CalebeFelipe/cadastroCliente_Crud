2bcdb8899fddaa8956bb6126d1499604664f09aa
Calebe Felipe <fcalebe7@gmail.com>
2022-05-20 12:04:39 -0300

    feat: Adiciona a função de Create

--- main.js
+++ main.js
@@ -6,6 +6,28 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    nome: "Calebe",
+    email: "fcalebe@gmail.com",
+    celular: "11123456789",
+    cidade: "Goiânia"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
+
+// CRUD - create read update delete
+
+
+// CRUD - CREATE
+const createClient = (client) => {
+    const db_client = getLocalStorage()
+    db_client.push(client)
+    setLocalStorage(db_client)
+}
+
+
+// Eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
